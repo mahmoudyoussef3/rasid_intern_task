eda05a563e9197532a6cb07793868dacf66b61eb
Mahmoud Youssef <mahmoudyousse220@gmail.com>
2024-12-13 18:28:51 +0200

    Enhance code and edit design.

--- lib/Features/Notifications/screens/notification.dart
+++ lib/Features/Notifications/screens/notification.dart
@@ -16,8 +16,6 @@ class NotificationScreen extends StatefulWidget {
 
 class _NotificationScreenState extends State<NotificationScreen> {
   DateTime? _selectedDateTime;
-  Duration _remainingTime = Duration.zero;
-  Timer? _timer;
   List<PendingNotificationRequest> _pendingNotifications = [];
 
   @override
@@ -29,104 +27,50 @@ class _NotificationScreenState extends State<NotificationScreen> {
   }
 
   Future<void> _requestNotificationPermission() async {
-    if (await Permission.notification.isGranted) {
-      debugPrint("Notification permission already granted.");
-      return;
-    }
     final status = await Permission.notification.request();
     if (status.isGranted) {
       debugPrint("Notification permission granted.");
-    } else if (status.isDenied) {
+    } else {
       debugPrint("Notification permission denied.");
-    } else if (status.isPermanentlyDenied) {
-      debugPrint("Notification permission permanently denied.");
-      openAppSettings();
+      if (status.isPermanentlyDenied) {
+        openAppSettings();
+      }
     }
   }
 
   Future<void> _scheduleNotification(DateTime scheduledTime) async {
-    final int notificationId = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-
-    const AndroidNotificationDetails androidDetails =
-        AndroidNotificationDetails(
-      'channel_id',
-      'channel_name',
+    const androidDetails = AndroidNotificationDetails(
+      'default_notification_channel_id',
+      'Default Notification Channel',
       importance: Importance.max,
       priority: Priority.high,
     );
 
-    const NotificationDetails notificationDetails = NotificationDetails(
-      android: androidDetails,
-    );
+    const notificationDetails = NotificationDetails(android: androidDetails);
 
     await LocalNotificationService.flutterLocalNotificationsPlugin
         .zonedSchedule(
-      notificationId,
+      3,
       'Scheduled Notification',
       'This notification is scheduled for ${scheduledTime.toString()}',
       tz.TZDateTime.from(scheduledTime, tz.local),
       notificationDetails,
       uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
-      androidScheduleMode: AndroidScheduleMode.alarmClock,
-      matchDateTimeComponents: DateTimeComponents.time, // Optional for daily
+          UILocalNotificationDateInterpretation.wallClockTime,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      payload: 'payloadData',
     );
 
     _checkPendingNotifications();
-    //  _startCountdown(scheduledTime);
   }
 
   Future<void> _checkPendingNotifications() async {
     final pendingNotifications = await LocalNotificationService
         .flutterLocalNotificationsPlugin
         .pendingNotificationRequests();
-
     setState(() {
       _pendingNotifications = pendingNotifications;
-      if (_pendingNotifications.isEmpty) {
-        // _resetCountdown();
-        _selectedDateTime = null;
-      }
     });
-  }
-
-  // void _startCountdown(DateTime scheduledTime) {
-  //   _timer?.cancel();
-  //   _updateRemainingTime(scheduledTime);
-  //
-  //   _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-  //     setState(() {
-  //       _updateRemainingTime(scheduledTime);
-  //     });
-  //   });
-  // }
-  //
-  // void _updateRemainingTime(DateTime scheduledTime) {
-  //   final now = DateTime.now();
-  //   if (now.isBefore(scheduledTime)) {
-  //     setState(() {
-  //       _remainingTime = scheduledTime.difference(now);
-  //     });
-  //   } else {
-  //     _resetCountdown();
-  //   }
-  // }
-  //
-  // void _resetCountdown() {
-  //   _timer?.cancel();
-  //   setState(() {
-  //     _remainingTime = Duration.zero;
-  //   });
-  // }
-
-  void _cancelNotification(int id) async {
-    await LocalNotificationService.flutterLocalNotificationsPlugin.cancel(id);
-    _checkPendingNotifications();
-  }
-
-  void _cancelAllNotifications() async {
-    await LocalNotificationService.flutterLocalNotificationsPlugin.cancelAll();
-    _checkPendingNotifications();
   }
 
   Future<void> _pickDateTime() async {
@@ -163,20 +107,6 @@ class _NotificationScreenState extends State<NotificationScreen> {
         }
       }
     }
-  }
-
-  // @override
-  // void dispose() {
-  //   _timer?.cancel();
-  //   super.dispose();
-  // }
-
-  String _formatDuration(Duration duration) {
-    final hours = duration.inHours;
-    final minutes = duration.inMinutes.remainder(60);
-    final seconds = duration.inSeconds.remainder(60);
-
-    return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
   }
 
   @override
@@ -216,35 +146,14 @@ class _NotificationScreenState extends State<NotificationScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(
-              height: 200,
-              child: AnalogClock(
-                  secondHandColor: Colors.white,
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 2.0, color: Colors.blue[800]!),
-                      color: Colors.transparent,
-                      shape: BoxShape.circle),
-                  width: 150.0,
-                  isLive: true,
-                  hourHandColor: Colors.blue[800]!,
-                  minuteHandColor: Colors.blue[800]!,
-                  showSecondHand: true,
-                  numberColor: Colors.blue[800]!,
-                  showNumbers: true,
-                  showAllNumbers: true,
-                  showTicks: true,
-                  showDigitalClock: true,
-                  datetime: DateTime.now()),
-            ),
+            _buildClock(),
             const SizedBox(height: 20),
             _buildActionButtons(),
             const SizedBox(height: 20),
             _buildClearAllNotificationsButton(),
             const SizedBox(height: 20),
             _pendingNotifications.isEmpty
-                ? const Center(
-                    child: Text('There is no pending notification'),
-                  )
+                ? const Center(child: Text('There is no pending notification'))
                 : _buildPendingNotificationsList(),
           ],
         ),
@@ -252,35 +161,32 @@ class _NotificationScreenState extends State<NotificationScreen> {
     );
   }
 
-  // Widget _buildCountdown() {
-  //   return Container(
-  //     decoration: BoxDecoration(
-  //       color: Colors.white,
-  //       borderRadius: BorderRadius.circular(16),
-  //       boxShadow: const [
-  //         BoxShadow(
-  //           color: Colors.black26,
-  //           blurRadius: 8,
-  //           offset: Offset(0, 4),
-  //         ),
-  //       ],
-  //     ),
-  //     padding: const EdgeInsets.symmetric(vertical: 20),
-  //     child: Center(
-  //       child: Text(
-  //         _remainingTime.inSeconds > 0
-  //             ? _formatDuration(_remainingTime)
-  //             : '00:00:00',
-  //         style: const TextStyle(
-  //           fontSize: 48,
-  //           fontWeight: FontWeight.bold,
-  //           color: Colors.blueAccent,
-  //           fontFamily: 'Courier',
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
+  Widget _buildClock() {
+    return SizedBox(
+      height: 200,
+      child: AnalogClock(
+        digitalClockColor: Colors.white,
+        tickColor: Colors.white,
+        secondHandColor: Colors.white,
+        decoration: BoxDecoration(
+          border: Border.all(width: 2.0, color: Colors.blue[800]!),
+          color: Colors.transparent,
+          shape: BoxShape.circle,
+        ),
+        width: 150.0,
+        isLive: true,
+        hourHandColor: Colors.blue[800]!,
+        minuteHandColor: Colors.blue[800]!,
+        showSecondHand: true,
+        numberColor: Colors.blue[800]!,
+        showNumbers: true,
+        showAllNumbers: true,
+        showTicks: true,
+        showDigitalClock: true,
+        datetime: DateTime.now(),
+      ),
+    );
+  }
 
   Widget _buildActionButtons() {
     return SizedBox(
@@ -291,96 +197,61 @@ class _NotificationScreenState extends State<NotificationScreen> {
         scrollDirection: Axis.horizontal,
         children: [
           _buildInstantNotificationButton(),
-          const VerticalDivider(
-            color: Colors.white,
-          ),
+          const VerticalDivider(color: Colors.white),
           _buildRepeatedNotificationButton(),
-          const VerticalDivider(
-            color: Colors.white,
-          ),
+          const VerticalDivider(color: Colors.white),
           _buildScheduleNotificationButton(),
-          const VerticalDivider(
-            color: Colors.white,
-          ),
+          const VerticalDivider(color: Colors.white),
         ],
       ),
     );
   }
 
   Widget _buildInstantNotificationButton() {
-    return Column(
-      children: [
-        ElevatedButton.icon(
-          onPressed: () {
-            LocalNotificationService.showBasicNotification(
-              1,
-              'Instant Notification',
-              'The body of the notification',
-            );
-          },
-          icon: const Icon(
-            Icons.notification_important,
-            color: Colors.white,
-          ),
-          label: Text(
-            'Instant',
-            style: GoogleFonts.montserrat(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blue[800],
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-        ),
-        ElevatedButton.icon(
-          onPressed: () {
-            _cancelNotification(1);
-          },
-          icon: const Icon(
-            Icons.cancel,
-            color: Colors.white,
-          ),
-          label: Text(
-            'Cancel',
-            style: GoogleFonts.montserrat(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.red[800],
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-        )
-      ],
-    );
+    return _buildNotificationButton('Instant', Icons.notification_important,
+        () {
+      LocalNotificationService.showBasicNotification(
+        1,
+        'Instant Notification',
+        'The body of the notification',
+      );
+    }, 1);
   }
 
   Widget _buildRepeatedNotificationButton() {
+    return _buildNotificationButton('Repeated', Icons.repeat, () {
+      LocalNotificationService.repeatedNotification();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.white,
+          content: Text(
+            'Notification repeated every minute',
+            style: TextStyle(
+                color: Colors.blue[800],
+                fontWeight: FontWeight.w600,
+                fontSize: 14),
+          ),
+        ),
+      );
+    }, 2);
+  }
+
+  Widget _buildScheduleNotificationButton() {
+    return _buildNotificationButton(
+        'Schedule', Icons.schedule, _pickDateTime, 3);
+  }
+
+  Widget _buildNotificationButton(
+      String label, IconData icon, VoidCallback onPressed, int id) {
     return Column(
       children: [
         ElevatedButton.icon(
-          onPressed: () {
-            LocalNotificationService.repeatedNotification();
-          },
-          icon: const Icon(
-            Icons.repeat,
-            color: Colors.white,
-          ),
+          onPressed: onPressed,
+          icon: Icon(icon, color: Colors.white),
           label: Text(
-            'Repeated',
+            label,
             style: GoogleFonts.montserrat(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+                fontWeight: FontWeight.bold, color: Colors.white),
           ),
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.blue[800],
@@ -392,18 +263,13 @@ class _NotificationScreenState extends State<NotificationScreen> {
         ),
         ElevatedButton.icon(
           onPressed: () {
-            _cancelNotification(2);
+            _cancelNotification(id);
           },
-          icon: const Icon(
-            Icons.cancel,
-            color: Colors.white,
-          ),
+          icon: const Icon(Icons.cancel, color: Colors.white),
           label: Text(
             'Cancel',
             style: GoogleFonts.montserrat(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+                fontWeight: FontWeight.bold, color: Colors.white),
           ),
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.red[800],
@@ -415,68 +281,16 @@ class _NotificationScreenState extends State<NotificationScreen> {
         )
       ],
     );
-  }
-
-  Widget _buildScheduleNotificationButton() {
-    return Column(children: [
-      ElevatedButton.icon(
-        onPressed: _pickDateTime,
-        icon: const Icon(
-          Icons.schedule,
-          color: Colors.white,
-        ),
-        label: Text(
-          'Schedule',
-          style: GoogleFonts.montserrat(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue[800],
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-      ),
-      ElevatedButton.icon(
-        onPressed: () {},
-        icon: const Icon(
-          Icons.cancel,
-          color: Colors.white,
-        ),
-        label: Text(
-          'Cancel',
-          style: GoogleFonts.montserrat(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.red[800],
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-      )
-    ]);
   }
 
   Widget _buildClearAllNotificationsButton() {
     return ElevatedButton.icon(
       onPressed: _cancelAllNotifications,
-      icon: const Icon(
-        Icons.clear_all,
-        color: Colors.white,
-      ),
+      icon: const Icon(Icons.clear_all, color: Colors.white),
       label: Text(
         'Clear All',
         style: GoogleFonts.montserrat(
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
+            fontWeight: FontWeight.bold, color: Colors.white),
       ),
       style: ElevatedButton.styleFrom(
         backgroundColor: Colors.blue[800],
@@ -503,9 +317,7 @@ class _NotificationScreenState extends State<NotificationScreen> {
               title: Text(
                 'Scheduled Notification ID: ${notification.id}',
                 style: GoogleFonts.montserrat(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14,
-                ),
+                    fontWeight: FontWeight.bold, fontSize: 14),
               ),
               trailing: IconButton(
                 icon: const Icon(Icons.cancel, color: Colors.red),
@@ -518,5 +330,15 @@ class _NotificationScreenState extends State<NotificationScreen> {
         },
       ),
     );
+  }
+
+  void _cancelNotification(int id) async {
+    await LocalNotificationService.flutterLocalNotificationsPlugin.cancel(id);
+    _checkPendingNotifications();
+  }
+
+  void _cancelAllNotifications() async {
+    await LocalNotificationService.flutterLocalNotificationsPlugin.cancelAll();
+    _checkPendingNotifications();
   }
 }
